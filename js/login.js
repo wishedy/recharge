ec4afa21a00801441c19c041d4f67fb7de918b81
zhangheng <zhangheng@allinmd.cn>
2017-09-21 09:26:52 +0800

    骰子充值效果前端效果实现

--- js/login.js
+++ js/login.js
@@ -7,6 +7,19 @@ $(document).ready(function(){
             var t = this;
             t.checkLogin();
         },
+        loginWord: [
+            {
+                id:20170921,
+                passWord:20170921
+            },
+            {
+                id:20170921,
+                passWord:20170921
+            },{
+                id:20170921,
+                passWord:20170921
+            }
+        ],
         loginTemplate:'<div class="recharge-login" style="display:block;">'+
         '    <div class="recharge-login-container">'+
         '        <header class="recharge-login-header">请登录账号</header>'+
@@ -25,17 +38,56 @@ $(document).ready(function(){
         '        </div>'+
         '    </div>'+
         '</div>',
+        dialogShow: function (obj) {
+            var t = this;
+            var maskObj = $(".mask-modal");
+            var popupContainer = $(".popupCont");
+            var titleBar = popupContainer.find('.title');
+            var contentBar = popupContainer.find(".content");
+            titleBar.html(obj.title);
+            contentBar.html(obj.text);
+            maskObj.show();
+            $(".closePopup").off("click").on("click", function () {
+                maskObj.hide();
+            });
+            $('.btnEnter').off("click").on("click", function () {
+                maskObj.hide();
+            });
+
+        },
         checkLogin:function(){
             var t = this;
+            // console.log(localStorage.getItem("login","true"))
             if(!t.loginOnOff){
+                $(".recharge-login").remove();
                 $("body").append(t.loginTemplate);
                 t.loginBegin();
+            }else{
+                return false;
             }
         },
         loginBegin:function(){
-            $("#login-user-id")
+            var t = this;
+            $(".recharge-login-btn").off("click").on("click",function(){
+                console.log("开始");
+                var loginId = parseInt($("#login-user-id").val(),10);
+                var loginPassword =parseInt($("#login-user-password").val(),10);
+                $.each(t.loginWord,function(i,v){
+                    if((v.id===loginId)){
+                        if(v.passWord===loginPassword){
+                            $(".recharge-login").remove();
+                            localStorage.setItem("login","true");
+                            t.dialogShow({
+                                title: "恭喜您登录成功！",
+                                text: "登录成功"
+                            });
+                            return false;
+                        }
+                    }
+                });
+            });
         },
         loginOnOff:localStorage.getItem("login")?true:false
-    }
+    };
     login.init();
 });
